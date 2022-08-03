fd03932746802a89b984922414b8f4a584b7f64a
Wagyourtail <wagyourtail@wagyourtail.xyz>
2022-08-03 17:07:50 -0600

    add alpha stuff

---
+++ src/main/java/net/minecraft/launchwrapper/AlphaVanillaTweaker.java
@@ -0,0 +1,35 @@
+//
+// Source code recreated from a .class file by Quiltflower
+//
+
+package net.minecraft.launchwrapper;
+
+import java.io.File;
+import java.util.List;
+
+public class AlphaVanillaTweaker implements ITweaker {
+    public static File gameDir;
+    public static File assetsDir;
+    private List<String> args;
+
+    public AlphaVanillaTweaker() {
+    }
+
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
+        this.args = args;
+        AlphaVanillaTweaker.gameDir = gameDir;
+        AlphaVanillaTweaker.assetsDir = assetsDir;
+    }
+
+    public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+        classLoader.registerTransformer("net.minecraft.launchwrapper.injector.AlphaVanillaTweakInjector");
+    }
+
+    public String getLaunchTarget() {
+        return "net.minecraft.launchwrapper.injector.AlphaVanillaTweakInjector";
+    }
+
+    public String[] getLaunchArguments() {
+        return (String[])this.args.toArray(new String[this.args.size()]);
+    }
+}
---
+++ src/main/java/net/minecraft/launchwrapper/VanillaTweaker.java
@@ -0,0 +1,35 @@
+//
+// Source code recreated from a .class file by Quiltflower
+//
+
+package net.minecraft.launchwrapper;
+
+import java.io.File;
+import java.util.List;
+
+public class VanillaTweaker implements ITweaker {
+    public static File gameDir;
+    public static File assetsDir;
+    private List<String> args;
+
+    public VanillaTweaker() {
+    }
+
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
+        this.args = args;
+        VanillaTweaker.gameDir = gameDir;
+        VanillaTweaker.assetsDir = assetsDir;
+    }
+
+    public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+        classLoader.registerTransformer("net.minecraft.launchwrapper.injector.VanillaTweakInjector");
+    }
+
+    public String getLaunchTarget() {
+        return "net.minecraft.client.Minecraft";
+    }
+
+    public String[] getLaunchArguments() {
+        return (String[])this.args.toArray(new String[this.args.size()]);
+    }
+}
